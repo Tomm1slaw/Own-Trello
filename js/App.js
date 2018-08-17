a6222bfa30ed39ea5c45e9f00b1b168d520f5f8b
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-08-17 02:16:40 +0200

    Finish task-12-7

---
+++ js/App.js
@@ -0,0 +1,53 @@
+'use strict;'
+
+// variables to connet with serwer
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+    'X-Client-Id': '3460',
+    'X-Auth-Token': 'c7a836759193b92cef451a2ac4526c2b',
+    'Content-Type': 'application/json; charset=utf-8'
+};
+
+// function to request serfver for resource array
+
+fetch(baseUrl + '/board', {
+        headers: myHeaders
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(resp) {
+        setupColumns(resp.columns);
+    });
+
+// function to create column
+
+function setupColumns(columns) {
+    columns.forEach(function(column) {
+        var col = new Column(column.id, column.name);
+        board.addColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+// function to create cards in column
+
+function setupCards(col, cards) {
+    cards.forEach(function(card) {
+        var cardObj = new Card(card.id, card.name);
+        col.addCard(cardObj);
+    });
+}
+
+// function to generate template from HTML
+
+function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || 'div');
+
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+
+    return element;
+}
